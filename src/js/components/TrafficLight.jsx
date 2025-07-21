4aef01ef29db46356555e764a907a6346617aa14
salemmohdmohd <salim.farouq@gmail.com>
2025-07-21 10:28:52 -0500

    simple onclickreact

---
+++ src/js/components/TrafficLight.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import ReactDOM from "react-dom";
+import "../../styles/index.css";
+
+const TrafficLight = () => {
+  const [currentLight, setCurrentLight] = useState("red");
+
+  // on click
+  const clickFunction = (color) => {
+    setCurrentLight(color);
+  };
+
+  return (
+    <div id="trafficTop">
+      <div className="Container">
+        <div
+          id="redlight"
+          className={`redLight ${currentLight === "red" ? "active" : ""}`}
+          onClick={() => clickFunction("red")}
+        ></div>
+        <div
+          id="yellowlight"
+          className={`yellowLight ${currentLight === "yellow" ? "active" : ""}`}
+          onClick={() => clickFunction("yellow")}
+        ></div>
+        <div
+          id="greenlight"
+          className={`greenLight ${currentLight === "green" ? "active" : ""}`}
+          onClick={() => clickFunction("green")}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
+
+export default TrafficLight;
